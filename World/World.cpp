5c51bda29deb16a4ed491c1badfab1addd2449e4
Mathias Neerup <manee12@student.sdu.dk>
2015-12-16 11:06:08 +0100

    Adding world

---
+++ World/World.cpp
@@ -0,0 +1,61 @@
+/*
+ * World.cpp
+ *
+ *  Created on: Dec 16, 2015
+ *      Author: exchizz
+ */
+
+#include "World.h"
+
+World::World(Image *rawImg) {
+	img = new Image(rawImg);
+
+	//img->imageData = rawImg->imageData;
+	// TODO Auto-generated constructor stub
+}
+
+
+void World::WallExpansion(){
+		int expansion_factor = 4;
+		std::vector<std::vector <int> > expand_map;
+
+
+		//Might optimize later
+		for(int i = 0; i < getWidth(); i++){
+			std::vector<int> col(getHeight());
+			expand_map.push_back(col);
+		}
+
+		for(int x = 0; x < img->getWidth(); x++){
+			for(int y = 0; y < img->getHeight(); y++){
+				if(img->getPixel(x,y) <= 50){ // Retter 50 senere
+					for(int i = 0; i <= expansion_factor;i++){
+						for(int j = 0; j <= expansion_factor;j++){
+							if(y+i <= img->getHeight()-1 && x+j <= img->getWidth()-1 ){
+								expand_map[x+j][y+i] = 1;
+							}
+							if(y+i <= img->getHeight()-1 && x-j >= 0){
+								expand_map[x-j][y+i] = 1;
+							}
+							if(y-i >= 0 && x-j >= 0 ){
+								expand_map[x-j][y-i] = 1;
+							}
+							if(y-i >= 0 && x+j <= img->getWidth()-1 ){
+								expand_map[x+j][y-i] = 1;
+							}
+						}
+					}
+				}else if(expand_map[x][y] != 1){
+						expand_map[x][y] = 0;
+				}
+			}
+		}
+
+		std::cout << "width: " << getWidth() << std::endl;
+		for(int x = 0; x < getWidth(); ++x){
+			for(int y = 0; y < getHeight(); y++){
+				//setPixel(x,y,(expand_map[x][y] == 0) ? WHITE : BLACK);
+				setPixel(x,y,expand_map[x][y]);
+			}
+		}
+}
---
+++ World/World.h
@@ -0,0 +1,21 @@
+/*
+ * World.h
+ *
+ *  Created on: Dec 16, 2015
+ *      Author: exchizz
+ */
+
+#ifndef WORLD_WORLD_H_
+#define WORLD_WORLD_H_
+
+#include "../Image/Image.h"
+
+class World {
+public:
+	Image *img;
+
+	World(Image *img);
+	void World::WallExpansion();
+};
+
+#endif /* WORLD_WORLD_H_ */
