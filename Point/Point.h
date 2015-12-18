935495ddf3530630756f563986d30b17d88f3309
Mathias Neerup <manee12@student.sdu.dk>
2015-12-18 13:24:53 +0100

    Ittr 36 with 1 double

--- Point/Point.h
+++ Point/Point.h
@@ -4,6 +4,7 @@ class Point {
 public:
 	signed int x_pos;
 	signed int y_pos;
+    bool visited = false;
 	Point();
         Point(signed int x, signed int y);
 };
--- Tree/Tree.cpp
+++ Tree/Tree.cpp
@@ -7,84 +7,71 @@ Tree::Tree()
 
 Tree::Tree(World* world)
 {
-    this->world = world;
+	this->world = world;
 }
 
 
 std::vector<Door> Tree::door_hitpoint_merge(std::vector<Door> & doorways, std::vector<Point> &wavefront_hitpoint_px){
-    std::vector<Door> output;
-    for (auto px : wavefront_hitpoint_px){
-        for(int i = 0; i < doorways.size(); ++i){
-            if(px.x_pos == doorways[i].px1.x_pos && px.y_pos == doorways[i].px1.y_pos){
-                 doorways[i].px1_visited = true;
-                 output.push_back(doorways[i]);
-            }
-            if(px.x_pos == doorways[i].px2.x_pos && px.y_pos == doorways[i].px1.y_pos){
-                 doorways[i].px2_visited = true;
-                 output.push_back(doorways[i]);
-            }
-        }
-    }
+	std::vector<Door> output;
+	bool check1 = false;
+	bool check2 = false;
 
-    return output;
+	for(int i = 0; i < doorways.size(); ++i){
+		for (auto px : wavefront_hitpoint_px){
+			//std::cout << "wavefront hitpoints: " << px.x_pos << ", " << px.y_pos << std::endl;
+			if(px.x_pos == doorways[i].px1.x_pos && px.y_pos == doorways[i].px1.y_pos){
+				doorways[i].px1.visited = true;
+				check1 = true;
+			}
+			if(px.x_pos == doorways[i].px2.x_pos && px.y_pos == doorways[i].px2.y_pos){
+				doorways[i].px2.visited = true;
+				check2 = true;
+			}
+		}
+
+		if(check1 || check2){
+			check1 = false;
+			check2 = false;
+			output.push_back(doorways[i]);
+		}
+	}
+
+	return output;
 }
 
-std::vector<Node> Tree::Tree_generator(Point start, std::vector<Door> &doorways){
+std::vector<Door> Tree::Tree_generator(Point &start, std::vector<Door> &doorways){
 
-    std::queue<Door> RoomToBeServed;
-    bool runonce = true;
-    int i = 4;
+	static int counter = 36;
 
-    while(!RoomToBeServed.empty() || runonce){
-        Door startFrom = RoomToBeServed.front();
-        RoomToBeServed.pop();
+	if(--counter <= 0){
+		std::vector<Door> rtn;
+		return rtn;
+	}
+	// find doors from offload station
+	auto hit_px_doors = world->Wavefront_DoorScanner(start, 127, 126);
 
-        Point startPoint;
+	// Merge doors with hitspoints
+	auto doorways_visited = door_hitpoint_merge(doorways, hit_px_doors);
 
-        if(startFrom.px1_visited){
-            startPoint = startFrom.px2; // Start from opposite site of door
-        } else if(startFrom.px2_visited){
-            startPoint = startFrom.px1;
-        } else {
-            std::cout << "Room with no doors" << std::endl;
-        }
+	Point *startPoint;
+	std::vector<Door> output;
+	for(auto &door: doorways_visited){
+		if(door.start.x_pos == 456){
+			std::cout << "point " << door.start.x_pos << "," << door.start.y_pos << std::endl;
+		}
+		if(door.px1.visited && door.px2.visited){
+			continue;
+		}
 
-        if(runonce){
-            startPoint = start;
-            runonce = false;
-        }
-
-        std::cout << "startign at (x,y): (" << startPoint.x_pos << "," << startPoint.y_pos << ")" << std::endl;
-
-        // find doors from offload station
-        auto hit_px_doors = world->Wavefront_DoorScanner(startPoint, 127, 126);
-
-
-        // Merge doors with hitspoints
-        auto doorways_visisted = door_hitpoint_merge(doorways, hit_px_doors);
-
-        //Run through every door and mark hitpoint
-        for (auto door : doorways_visisted){
-            if(door.px1_visited){
-                world->img->setPixel(door.px1.x_pos, door.px1.y_pos, 2887);
-                RoomToBeServed.push(door);
-
-            } else if(door.px2_visited){
-                world->img->setPixel(door.px2.x_pos, door.px2.y_pos, 2887);
-                RoomToBeServed.push(door);
-
-            }
-
- //           std::cout << "qeue size: " << RoomToBeServed.size() << std::endl;
-        }
-
-        if(--i == 0){
-            std::vector<Node> output;
-            return output;
-        }
-
-    }
-
-    std::vector<Node> output;
-    return output;
+		if(door.px1.visited){
+			startPoint = &door.px2; // Start from opposite site of door
+		} else if(door.px2.visited){
+			startPoint = &door.px1;
+		} else {
+			std::cout << "Room with no doors" << std::endl;
+		}
+		door.adjacent = Tree_generator(*startPoint, doorways);
+		output.push_back(door);
+	}
+	return output;
 }
